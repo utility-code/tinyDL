43ffde798713f6f2f666712a99a43dbb35581344
SubhadityaMukherjee <msubhaditya@gmail.com>
2021-04-04 16:04:50 +0400

    site_test

--- pusher.sh
+++ pusher.sh
@@ -1,5 +1,5 @@
 black "."
-pdoc --html tinydl
+pdoc --o docs tinydl
 jupytext --to notebook "demo.py"
 if [[ ! -z $1 ]]; then
         git add . && git commit -m $1 && git push
--- tinydl/__init__.py
+++ tinydl/__init__.py
@@ -1,1 +1,4 @@
+"""[summary]
+This Library is a test
+"""
 from .tensor import Tensor as Tensor

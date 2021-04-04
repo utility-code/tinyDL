c8d775c2503820b6828916bb673bc483dbf3fa12
SubhadityaMukherjee <msubhaditya@gmail.com>
2021-04-04 16:25:18 +0400

    test

--- pusher.sh
+++ pusher.sh
@@ -1,5 +1,7 @@
 black "."
 pdoc --o docs tinydl
+mv docs/tinydl/index.html docs/index.md
+mv docs/tinydl/* docs/
 jupytext --to notebook "demo.py"
 if [[ ! -z $1 ]]; then
         git add . && git commit -m $1 && git push
